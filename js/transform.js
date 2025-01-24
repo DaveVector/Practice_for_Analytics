b6aa700c3c0d06236e915f6f09c9c59598b154f6
Vector <rappelz.sa@gmail.com>
2025-01-25 01:23:56 +0300

    update pages

---
+++ js/transform.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = document.querySelector('.slider');
+  const slides = document.querySelectorAll('.slide');
+  let currentIndex = 0;
+
+  function switchSlide() {
+    currentIndex = (currentIndex + 1) % slides.length; // Обчислюємо індекс слайду
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }
+
+  // Перемикання кожні 5 секунд
+  setInterval(switchSlide, 5000);
+});
+
